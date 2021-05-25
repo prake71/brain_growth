2e7d0e0e7e8bcbf0cfdf458e3f9e594609af1f83
Peter Rake <prake71@gmail.com>
2021-05-25 23:16:24 +0200

    flip card

--- Card.gd
+++ Card.gd
@@ -4,6 +4,8 @@ extends TextureButton
 var value
 var face = preload("res://assets/cards/card-1.png")
 var back = preload("res://assets/cards/card-back.png")
+var face_up = false
+
 
 func init(var v=1):
 	value = v
@@ -17,3 +19,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _pressed():
+	if get_normal_texture() == back:
+		set_normal_texture(face)
+		face_up = true
+	else:
+		set_normal_texture(back)
+		face_up = false
+		
